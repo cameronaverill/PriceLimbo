a346fede84e504d0ed0cb973ff0ec4903375cfbb
cameronaverill <cameronaverill2018@u.northwestern.edu>
2016-01-20 21:23:58 -0500

    in the midst of processing the shoes

--- src/Example1.java
+++ src/Example1.java
@@ -5,21 +5,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Example1{  
-    public static void main( String[] args ) throws IOException{  
-    	try{
-    			//maxBodySize prevents timeout
-                Document doc = Jsoup.connect("http://www.freepeople.com/shoes/"
-                		+ "?browseState=updated&hasLeftNav=YES&startResult=1&showAll=1&sizes=lt").maxBodySize(0).get(); 
-                //get all elements with a div attribute data-stylenumber
-                Elements items = doc.select("div[data-stylenumber]");
-                for (Element el : items){
-                	System.out.println("li: " + el.text());
-                }
-                System.out.println(items.size());
-    		}
-    	catch (IOException e)
-    	{
-    	    e.printStackTrace();
-    	}
-}
+    public static void main( String[] args ){  
+    	FreePeopleShoeParser fp = new FreePeopleShoeParser();
+    	System.out.println("done");
+    }
 }
--- src/FreePeopleShoeParser.java
+++ src/FreePeopleShoeParser.java
@@ -1,30 +1,62 @@
 import java.io.IOException;  
-import org.jsoup.Jsoup;  
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class FreePeopleShoeParser {
 	//link to access FreePeople data
 	 private final String LINK = "http://www.freepeople.com/shoes/"
 			 + "?browseState=updated&hasLeftNav=YES&startResult=1&showAll=1&sizes=lt";
+	 //map the shoe id to the FreePeopleShoe it represents
+	 private static HashMap<String, FreePeopleShoe> shoeMap = new HashMap<String, FreePeopleShoe>();
 	 
 	 public FreePeopleShoeParser(){
 		 try{
+			 Elements allShoes = getAllShoes();
+			 for(Element shoe : allShoes){
+				 createShoe(shoe);
+			 }
+		 }
+		 catch(IOException e){
+			 e.printStackTrace();
+			 //send me an email to say it stopped working eventually
+			 
+			 return;
+		 }
+		 
+	 }
+	 
+	 //gets all the items on the page
+	 private Elements getAllShoes() throws IOException{
+		try{
  			//maxBodySize prevents timeout
              Document doc = Jsoup.connect(LINK).maxBodySize(0).get(); 
-             
              //get all elements with a div attribute data-stylenumber
-             Elements items = doc.select("div[data-stylenumber]");
-             for (Element el : items){
-             	System.out.println("li: " + el.text());
-             }
-             System.out.println(items.size());
+             Elements items = doc.getElementsByAttribute("data-stylenumber");
+             return items;
  			}
 		 catch (IOException e)
 		 {
-			 e.printStackTrace();
+			 throw new IOException("The link could not be processed"); 
 		 }
 	}
+	 
+	 private void createShoe(Element shoe){
+		 //.attr returns an empty string if there isn't one
+		 String product_id = shoe.attr("data-stylenumber");
+		 //don't want to process again if we already have it in the map
+		 if(shoeMap.containsKey(product_id)){
+			 return;
+		 } 
+		 Attributes atts = shoe.attributes();
+		 //iterate through all the attributes
+		 for(Attribute a : atts){
+			 
+		 }
+		 
+	 }
 	 
 }
